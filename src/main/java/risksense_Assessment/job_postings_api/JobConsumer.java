0525b6127d764041a13e3e6fde0cc0a59bd9b4bc
Santhosh <Santhoshkamishetty.27@gmail.com>
2018-06-13 16:26:52 -0500

    created Search API, Integrated AWS RDS

--- src/main/java/risksense_Assessment/job_postings_api/JobConsumer.java
+++ src/main/java/risksense_Assessment/job_postings_api/JobConsumer.java
@@ -1,5 +1,9 @@
 package risksense_Assessment.job_postings_api;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Collections;
 import java.util.Properties;
 
@@ -11,7 +15,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 
 public class JobConsumer {
     
-    static void executeConsumer() throws InterruptedException {
+    static void executeConsumer(Connection connection) throws InterruptedException {
     	final Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "jobConsumer");
@@ -32,7 +36,14 @@ public class JobConsumer {
                 else continue;
             }
             consumerRecords.forEach(record -> {
-            		System.out.println(record.value().getRole()+ " "+ record.value().getAvailability());
+                try {
+					String query = "INSERT INTO `JobPostings`.`postings` (`id`, `role`, `compensation`, `location`, `description`, `reply_rate`, `skills`, `availability`, `posted_date`)"
+							+ " VALUES ('"+ record.value().getId()+"', '"+ record.value().getRole()+"', '"+ record.value().getCompensation()+"', '"+ record.value().getLocation()+"', '"+  record.value().getDescription()+"', '"+  record.value().getReplyRate()+"', '"+ record.value().getSkills()+"', '"+ record.value().getAvailability() +"', '"+  record.value().getPosted_date()+"' )";  
+					Statement stmt=connection.createStatement(); 
+					stmt.executeUpdate(query);
+                } catch (SQLException e) {
+					e.printStackTrace();
+				} 
             });
              
             consumer.commitAsync();
@@ -41,6 +52,14 @@ public class JobConsumer {
     }
     
     public static void main(String[] args) throws Exception {
-    	executeConsumer();
+    	try {
+    		Class.forName("com.mysql.cj.jdbc.Driver");
+    	} catch (ClassNotFoundException e) {
+    		e.printStackTrace();
+    		return;
+    	}
+    	Connection connection = DriverManager.getConnection("jdbc:mysql://santhosh.crzbyoydhnfc.us-east-2.rds.amazonaws.com:3306/JobPostings?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=CST","root", "password");		
+        executeConsumer(connection);
+        connection.close();
     }
 }
--- src/main/java/risksense_Assessment/job_postings_api/MainVerticle.java
+++ src/main/java/risksense_Assessment/job_postings_api/MainVerticle.java
@@ -1,12 +1,19 @@
 package risksense_Assessment.job_postings_api;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
@@ -14,34 +21,64 @@ import io.vertx.kafka.client.producer.KafkaProducer;
 import io.vertx.kafka.client.producer.KafkaProducerRecord;
 
 public class MainVerticle extends AbstractVerticle {
-	
-	
-public void pushKafka(JobEntity jobEntity){
-	Map<String, String> config = new HashMap<>();
-	  config.put("bootstrap.servers", "localhost:9092");
-	  config.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-	  config.put("value.serializer", "io.vertx.blog.my_first_app.JobEntitySerializer");
-	  config.put("acks", "1");
+		
+	public void pushKafka(JobEntity jobEntity){
+		Map<String, String> config = new HashMap<>();
+		  config.put("bootstrap.servers", "localhost:9092");
+		  config.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		  config.put("value.serializer", "io.vertx.blog.my_first_app.JobEntitySerializer");
+		  config.put("acks", "1");
 
-	  KafkaProducer<String, JobEntity> producer = KafkaProducer.create(vertx, config);
-	  KafkaProducerRecord<String, JobEntity> record = KafkaProducerRecord.create("job_postings_1110am", jobEntity);
-	  producer.write(record);
-}
-
-
-
-private void addOne(RoutingContext routingContext) {
-	  final JobEntity jobEntity = Json.decodeValue(routingContext.getBodyAsString(), JobEntity.class);
-	  pushKafka(jobEntity);
-      routingContext.response()
-	      .setStatusCode(201)
-	      .putHeader("content-type", "application/json; charset=utf-8")
-	      .end(Json.encodePrettily(jobEntity));
-
-      System.out.println("pushed");
+		  KafkaProducer<String, JobEntity> producer = KafkaProducer.create(vertx, config);
+		  KafkaProducerRecord<String, JobEntity> record = KafkaProducerRecord.create("job_postings_1110am", jobEntity);
+		  producer.write(record);
 	}
 	
 
+	private void addOne(RoutingContext routingContext) {
+		  final JobEntity jobEntity = Json.decodeValue(routingContext.getBodyAsString(), JobEntity.class);
+		  pushKafka(jobEntity);
+	      routingContext.response()
+		      .setStatusCode(201)
+		      .putHeader("content-type", "application/json; charset=utf-8")
+		      .end(Json.encodePrettily(jobEntity));
+
+	      System.out.println("pushed");
+		}
+	
+
+	private void searchAll(RoutingContext routingContext) throws SQLException {
+		 try {
+	    		Class.forName("com.mysql.cj.jdbc.Driver");
+	    	} catch (ClassNotFoundException e) {
+	    		e.printStackTrace();
+	    		return;
+	    	}
+      Connection connection = DriverManager.getConnection("jdbc:mysql://santhosh.crzbyoydhnfc.us-east-2.rds.amazonaws.com:3306/JobPostings?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=CST","root", "password");		
+      
+      String keyword = routingContext.request().getParam("keyword");
+      String query = "SELECT * FROM postings WHERE MATCH (role,compensation,location,description,reply_rate,skills,availability) AGAINST ('"+keyword+"' IN NATURAL LANGUAGE MODE);";  
+	  Statement stmt=connection.createStatement(); 
+	  ResultSet rs = stmt.executeQuery(query);
+	  Map<Integer, JsonObject> searchedJobs = new LinkedHashMap<>();
+	  while(rs.next()){
+		  JsonObject tmp = new JsonObject();
+		  tmp.put("availability", rs.getString(8));
+		  tmp.put("compensation", rs.getString(3));
+		  tmp.put("description", rs.getString(5));
+		  tmp.put("location", rs.getString(4));
+		  tmp.put("posted_date", rs.getString(9));
+		  tmp.put("replyRate",rs.getString(6));
+		  tmp.put("role",rs.getString(2));
+		  tmp.put("skills",rs.getString(7));
+		  tmp.put("id",rs.getString(1));
+		 searchedJobs.put(rs.getInt(1), tmp);
+	  }  
+	  connection.close();
+      routingContext.response()
+      .putHeader("content-type", "application/json; charset=utf-8")
+      .end(Json.encodePrettily(searchedJobs.values())); 
+	}
 	
 	@Override
 	public void start(Future<Void> fut) {
@@ -59,7 +96,13 @@ private void addOne(RoutingContext routingContext) {
 	 
 	router.route("/api/job*").handler(BodyHandler.create());
 	router.post("/api/job").handler(this::addOne);
-	
+	router.get("/api/search/:keyword").handler(event -> {
+		try {
+			searchAll(event);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	});
 	vertx
 	     .createHttpServer()
 	     .requestHandler(router::accept)
